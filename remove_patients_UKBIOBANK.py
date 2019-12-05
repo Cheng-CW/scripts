44154c0c740cb8fd9d781ddc7339090c51bd456c
Chew Weng Cheng <C.W.CHENG@LEEDS.AC.UK>
2019-12-05 13:23:51 +0000

    Add files via upload
    This script is intended to manipulate clinical categories of patients in UK Biobank. The script will remove specific category according to the data-coding you specified.

---
+++ remove_patients_UKBIOBANK.py
@@ -0,0 +1,16 @@
+#python filter_specific.py id_20002.csv datacoding(1074) output.csv
+
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+input=sys.argv[1]
+field=sys.argv[2]
+output_name=sys.argv[3]
+
+PID = pd.read_csv(input, index_col=0)
+print("dataset:", PID.shape)
+selected=PID[(PID.iloc[0:, 0:] != int(field)).all(axis=1)]
+selected.to_csv(output_name)
+print("final dataset:", selected.shape)
